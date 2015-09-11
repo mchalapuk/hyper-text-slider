45f04df1816dc8f4cb91cecbdbf68b9fcf3b9311
Maciej Chałapuk <maciej@chalapuk.pl>
2015-09-11 06:32:27 +0200

    ~ slider implemented in node

---
+++ src/node/slider.js
@@ -0,0 +1,213 @@
+'use strict';
+
+var dom = require('./_dom');
+var hermes = require('./hermes');
+var precond = require('precond');
+
+function initializeSlider(elem) {
+  precond.checkArgument(elem instanceof Element, 'elem is not an instance of Element');
+
+  var priv = {};
+  priv.hermes = hermes(elem);
+  priv.slides = searchForSlides(elem);
+  priv.transitions = searchForTransitions(elem);
+  priv.fromIndex = 1;
+  priv.toIndex = 0;
+  priv.chooseTransition = chooseTransition;
+
+  priv.elem.className = priv.elem.className.replace(Regexp.TRANSITION, '');
+  setOptions(priv);
+  setTimeout(start.bind(priv), 100);
+
+  var pub = {};
+  pub.moveTo = moveTo.bind(priv);
+  pub.moveToNext = moveToNext.bind(priv);
+  pub.moveToPrevious = moveToPrevious.bind(priv);
+  return pub;
+}
+
+module.exports = initializeSlider;
+
+// constants
+
+var Layout = {
+  SLIDER: 'hermes-layout--slider',
+  SLIDE: 'hermes-layout--slide',
+  BACKGROUND: 'hermes-layout--background',
+  CONTENT: 'hermes-layout--content',
+  INNER: 'hermes-layout--inner',
+  ARROW_LEFT: 'hermes-layout--arrow-left',
+  ARROW_RIGHT: 'hermes-layout--arrow-right',
+  DOTS: 'hermes-layout--dots',
+  DOT: 'hermes-layout--dot',
+};
+
+var Selector = (function () {
+  var selectors = {};
+  for (var name in Layout) {
+    selectors[name] = '.' + Layout[name];
+  }
+  return selectors;
+}());
+
+var Option = {
+  DEFAULTS: 'hermes-defaults',
+  AUTOPLAY: 'hermes-autoplay',
+  CREATE_ARROWS: 'hermes-create-arrows',
+  CREATE_DOTS: 'hermes-create-dots',
+  ARROW_KEYS: 'hermes-arrow-keys',
+};
+
+var Flag = {
+  SLIDE_FROM: 'hermes-slide-from',
+  SLIDE_TO: 'hermes-slide-to',
+  UPGRADED: 'is-upgraded',
+  ACTIVE: 'is-active',
+};
+
+var Regexp = {
+  TRANSITION: new RegExp('hermes-transition--([^ ]+)', 'g'),
+};
+
+return;
+
+// initialization functions
+
+function searchForSlides(elem) {
+  return elem.querySelectorAll(Selector.SLIDE);
+}
+
+function searchForTransitions(elem) {
+  var transitions = [];
+  var matches = elem.className.match(Regexp.TRANSITION);
+  if (matches) {
+    for (var i = 0; i < matches.length; ++i) {
+      transitions.push(matches[i]);
+    }
+  }
+  return transitions;
+}
+
+function setOptions(priv) {
+  var cl = priv.elem.classList;
+
+  if (cl.contains(Option.DEFAULTS)) {
+    cl.add(Option.AUTOPLAY);
+    cl.add(Option.ARROW_KEYS);
+    cl.add(Option.CREATE_ARROWS);
+    cl.add(Option.CREATE_DOTS);
+  }
+
+  if (cl.contains(Option.CREATE_ARROWS)) {
+    createArrowButtons(priv);
+  }
+  if (cl.contains(Option.CREATE_DOTS)) {
+    createDotButtons(priv);
+  }
+  if (cl.contains(Option.ARROW_KEYS)) {
+    window.addEventListener('keydown', keyBasedMove.bind(priv));
+  }
+}
+
+function createArrowButtons(priv) {
+  var previousButton = dom.create(Layout.ARROW_LEFT);
+  previousButton.addEventListener('click', moveToPrevious.bind(priv));
+  priv.elem.appendChild(previousButton);
+
+  var nextButton = dom.create(Layout.ARROW_RIGHT);
+  nextButton.addEventListener('click', moveToNext.bind(priv));
+  priv.elem.appendChild(nextButton);
+}
+
+function createDotButtons(priv) {
+  var dots = dom.create(Layout.DOTS);
+  priv.elem.appendChild(dots);
+
+  for (var i = 0; i < priv.slides.length; ++i) {
+    var dot = dom.create(Layout.DOT);
+    dot.addEventListener('click', moveTo.bind(priv));
+    dots.appendChild(dot);
+    priv.slides[i].dot = dot;
+  }
+}
+
+function keyBasedMove(event) {
+  var priv = this;
+
+  switch (event.key) {
+    case 'ArrowLeft': moveToPrevious.bind(priv); break;
+    case 'ArrowRight': moveToNext.bind(priv); break;
+  }
+}
+
+function start() {
+  // separate start procedure is needed because
+  // only one slide is seen in the first transition
+  var priv = this;
+
+  var to = priv.slides[priv.toIndex];
+  to.classList.add(Flag.SLIDE_TO);
+  to.dot.classList.add(Flag.ACTIVE);
+
+  priv.elem.classList.add(priv.chooseTransition());
+
+  priv.hermes.startTransition();
+}
+
+// slide change functions
+
+function moveToNext() {
+  var priv = this;
+  priv.moveTo((priv.toIndex + 1) % priv.slides.length);
+}
+
+function moveToPrevious() {
+  var priv = this;
+  priv.moveTo((priv.toIndex - 1 + priv.slides.length) % priv.slides.length);
+}
+
+function moveTo(i) {
+  var priv = this;
+  precond.checkIsNumber(i, 'given index is not a number');
+
+  if (i <= priv.slides.length) {
+    i %= priv.slides.length;
+  }
+  if (i === priv.toIndex) {
+    return;
+  }
+
+  var from = priv.slides[priv.fromIndex];
+  var to = priv.slides[priv.toIndex];
+  dom.removeLayout(from, Flag.SLIDE_FROM);
+  dom.removeLayout(to, Flag.SLIDE_TO);
+  dom.removeLayout(to.dot, Flag.ACTIVE);
+  dom.removeLayout(priv.elem, Regexp.TRANSITION);
+
+  priv.fromIndex = priv.toIndex;
+  priv.toIndex = i;
+  from = priv.slides[priv.fromIndex];
+  to = priv.slides[priv.toIndex];
+  from.classList.add(Flag.SLIDE_FROM);
+  to.classList.add(Flag.SLIDE_TO);
+  to.dot.classList.add(Flag.ACTIVE);
+
+  priv.elem.classList.add(priv.chooseTransition());
+  priv.hermes.startTransition();
+}
+
+// transition change functions
+
+function chooseTransition() {
+  var priv = this;
+  var match = priv.slides[priv.toIndex].className.match(Regexp.TRANSITION);
+  return match[0] || random(priv.transitions);
+}
+
+function random(array) {
+  if (array.length === 0) {
+    throw "no transitions declared on slider";
+  }
+  return array[parseInt(Math.random() * array.length)];
+}
+
---
+++ src/node/slider.spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var slider = require('./slider');
+
+describe('Slider', function() {
+
+  var illegalArgs = [
+    { name: 'undefined', value: undefined, },
+    { name: 'null', value: null, },
+    { name: 'not element', value: [], },
+  ];
+
+  illegalArgs.forEach(function(arg) {
+    describe('given '+ arg.name +' argument,', function() {
+      describe('when created', function() {
+        it('throws exception', function() {
+          expect(function() { slider(arg.value); }).toThrow();
+        });
+      });
+    });
+  });
+});
+
